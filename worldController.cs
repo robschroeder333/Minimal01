dd3a656eff7f8b63a4af890a2d11bdb5806e087c
robschroeder333 <theguyonthecouch7@gmail.com>
2018-09-09 14:39:13 -0700

    feat: begin world generation

--- worldController.cs
+++ worldController.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class worldController : MonoBehaviour {
+public GameObject prefab;
+public float gridX = 5f;
+public float gridY = 5f;
+public float spacing = 1.5f;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+void Start() {
+    for (float y = 0; y < gridY; y++) {
+        for (float x = 0; x < gridX; x++) {
+            float offsetX = x - Mathf.Floor(gridX / 2);
+			Vector2 pos = new Vector2(offsetX, -y) * spacing;
+            Instantiate(prefab, pos, Quaternion.identity);
+        }
+    }
+} 
 	
 	// Update is called once per frame
 	void Update () {
